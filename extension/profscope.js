60dd00aef1450c94b3ffc988501f3bd6f4588c47
Matt <24287049+mattlul@users.noreply.github.com>
2018-10-19 00:42:21 -0400

    Began implementation of xhttp requests

--- extension/profscope.js
+++ extension/profscope.js
@@ -1,7 +1,6 @@
-/*
-TODO:
-    - Remove console.log debug messages
-*/
+
+// Initialize a collection that will be used to keep track of currently collected professors
+var currentProfCollection = [];
 
 if(document.title == "Look Up Classes"){
     // Gives us the HTMLCollection of elements with the class "datadisplaytable"  
@@ -9,30 +8,21 @@ if(document.title == "Look Up Classes"){
 
     // One table exists per MSU Selection Page, so only run code if length == 1
     if(elements.length == 1){
-        console.log("in 1");
         // This is the summary for each table on each MSU Selection Page
         if(elements[0].summary == "This layout table is used to present the sections found"){
-            console.log("in 2");
             // The element (table element) has two children on each MSU Selection Page
             if(elements[0].children.length == 2){
-                console.log("in 3");
-                console.log(elements[0].children.length);
-                performLinking(elements[0].children[1]);
-            }else{
-                console.log("Not enough children");
+                // Clear n
+                currentProfCollection = [];
+                professorLinker(elements[0].children[1]);
             }
-        }else{
-            console.log("Wrong summary");
         }
-    }else{
-        console.log("No table element");
     }
-}else{
-    console.log("Wrong Page Title");
 }
 
-function performLinking(mainTbody){
-    console.log("main tbody: " + mainTbody.children.length);
+
+function professorLinker(mainTbody){
+    var collectedProfs = [];
 
     // If the mainTbody element only has 2 children, there are no class listings
     if(mainTbody.children.length < 2){
@@ -45,8 +35,6 @@ function performLinking(mainTbody){
     // This element is the column header on the table
     var columnHeaderElement = mainTbody.children[1];
     
-    console.log("columnHeader " + columnHeaderElement.children.length);
-
     // Loop through the children of the column header element 
     for(var i = 0; i < columnHeaderElement.children.length; i++){
         // If the text of the column header child is equal to "Instructor", save that index of the child
@@ -56,23 +44,72 @@ function performLinking(mainTbody){
         }
     }
 
-    console.log("Professor Index: " + professorIndex);
-
     // Start from the third child (index 2). The first two children are always non-course children on MSU Selection page
     for(var x = 2; x < mainTbody.children.length; x++){
         var element = mainTbody.children[x];
 
         var professor = element.children[professorIndex].textContent;
 
+        professor = professor.substring(0, professor.indexOf("(")).trim().split(" ");
         console.log(professor);
+        if(professor.length == 4){
+            var firstName = professor[0].toString();
+            var lastName = professor[3].toString();
+            var fullName = lastName + " " + firstName;
 
+            //TODO: Add checks/manipulation for names with '-' in them
+            if(!collectedProfs.includes(fullName) && (!fullName.includes('-'))){
+                // Link
+                getRMPLink(firstName, lastName);
+                collectedProfs.push(fullName);
+                
+            }
+        }
         /*
          * TODO:
-         *  Need to get rid of the (P) at the end (OR ANY LETTER AT END) and need to remove white spaces
-         *  Need to queue into a search on RateMyProfessor  (maybe a new function)
          *  If the link/page is FOUND, need to bold the text, make it blue, and then link it
          */
     }
+}
 
+function getRMPLink(firstName, lastName){
+    var xhttp = new XMLHttpRequest();
+    var method = "POST";
+    
+    // Make sure the URL has "https" in it to avoid MixedContent... error
+    var url = "https://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=montclair%20state%20university&queryoption=HEADER&query=" 
+        + lastName + "%20" + firstName + "&facetSearch=true";
+    
+    var response = "";
+    
+    // On load of the xhttp request, set response equal to the responseText of the xhttp request
+    xhttp.onload = function(){
+        response = xhttp.responseText;
+    };
+    // Initiate the request
+    xhttp.open(method, url, true);
+    xhttp.send('');
 
+    // A short delay to simply make sure that xhttp.onload() has fully been called --> prevent response from being undefined
+    setTimeout(function(){
+        // Create a temporary div that will act as the holder for retrieving the direct URL for the professors RMP page
+        var div = document.createElement('div');
+        // Remove all (/g) script tags within the response text --> greatly reduces the size of the responseText
+        response = response.replace(/<script(.|\s)*?\/script>/g, '');
+        div.innerHTML = response;
+        // The "listing PROFESSOR" class houses the results on the page of our xhttp request
+        var listingsClass = div.getElementsByClassName("listing PROFESSOR")[0];
+        // Check to make sure that the first element of the HTMLCollection is not undefined
+        if(listingsClass != undefined){
+            // Check to make sure that the first element of HTMLCollection is not undefined
+            listingsClass = listingsClass.getElementsByTagName('a')[0];
+            if(listingsClass != undefined){
+                // The specific url is: RMP.com + the href of the listing
+                var specificURL =  "http://www.ratemyprofessors.com" + listingsClass.getAttribute('href');
+                console.log(specificURL);  
+            }
+        }else{
+            console.log("Error on EMP Listing --> Prof. reviews do not exist?");
+        }
+    }, 500);
 }
