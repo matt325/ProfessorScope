7588fb280d84db617555e1a472d4a83c14950d4c
Matt <24287049+mattlul@users.noreply.github.com>
2018-10-20 13:57:38 -0400

    Implemented pre-alpha linking concept

--- extension/profscope.js
+++ extension/profscope.js
@@ -1,6 +1,6 @@
 
 // Initialize a collection that will be used to keep track of currently collected professors
-var currentProfCollection = [];
+//var currentProfCollection = [];
 
 if(document.title == "Look Up Classes"){
     // Gives us the HTMLCollection of elements with the class "datadisplaytable"  
@@ -13,16 +13,20 @@ if(document.title == "Look Up Classes"){
             // The element (table element) has two children on each MSU Selection Page
             if(elements[0].children.length == 2){
                 // Clear n
-                currentProfCollection = [];
+                //currentProfCollection = [];
                 professorLinker(elements[0].children[1]);
             }
         }
     }
 }
 
-
-function professorLinker(mainTbody){
-    var collectedProfs = [];
+/**
+ * Extracts each professor from the MSU selection page for that course and links them with their RMP review link/scores
+ * Async so that we can utilize the await feature -> Used with our Promise handling
+ * @param {object} mainTbody 
+ */
+async function professorLinker(mainTbody){
+    var collectedProfsMap = new Map();
 
     // If the mainTbody element only has 2 children, there are no class listings
     if(mainTbody.children.length < 2){
@@ -48,30 +52,53 @@ function professorLinker(mainTbody){
     for(var x = 2; x < mainTbody.children.length; x++){
         var element = mainTbody.children[x];
 
-        var professor = element.children[professorIndex].textContent;
+        var professorElement = element.children[professorIndex];
+        var professorRoughText = professorElement.textContent;
+        
+        // Skip the code if the text is simply a "TBA" placeholder
+        if(professorRoughText === "TBA") continue;
 
-        professor = professor.substring(0, professor.indexOf("(")).trim().split(" ");
-        console.log(professor);
+        //var professor = professorElement.textContent;
+        var professor = professorRoughText.substring(0, professorRoughText.indexOf("(")).trim().split(" ");
+        //console.log(professor);
         if(professor.length == 4){
             var firstName = professor[0].toString();
             var lastName = professor[3].toString();
             var fullName = lastName + " " + firstName;
 
-            //TODO: Add checks/manipulation for names with '-' in them
-            if(!collectedProfs.includes(fullName) && (!fullName.includes('-'))){
-                // Link
-                getRMPLink(firstName, lastName);
-                collectedProfs.push(fullName);
-                
+            // If the Map contains a professor by their full name, no need to repeat the link search process
+                //TODO: Add checks/manipulation for names with '-' in them
+            if(!collectedProfsMap.has(fullName) && (!fullName.includes('-'))){
+                // Try to get the link. An error will be thrown if a link does not exist (from the reject(...) in the promise)
+                try {
+                    /*
+                        Get the professor-RMP link by running the getRMPLink function. 
+                        We await() so that a return value is given after the function is completely done.
+                    */
+                    var profRMPLink = await getRMPLink(firstName, lastName);
+                }catch(error){
+                    // If a professor does not have a RMP link, store their name in the map with the value of "no_link"
+                    collectedProfsMap.set(fullName, "no_link");
+                    console.log("Error on EMP Listing --> Prof. reviews do not exist?");
+                    continue;
+                }
+
+                console.log(profRMPLink);
+                // Save the professor (by full name) and their RMP link in a Map
+                collectedProfsMap.set(fullName, profRMPLink);
+            }
+            if(collectedProfsMap.get(fullName) !== "no_link"){
+                modifyElement(professorElement, profRMPLink, professorRoughText);
             }
         }
-        /*
-         * TODO:
-         *  If the link/page is FOUND, need to bold the text, make it blue, and then link it
-         */
     }
 }
 
+/**
+ * Retrieves the RateMyProfessor link (if it exists) for the professor with (firstName+lastName) as a name
+ * @param {string} firstName 
+ * @param {string} lastName 
+ */
 function getRMPLink(firstName, lastName){
     var xhttp = new XMLHttpRequest();
     var method = "POST";
@@ -90,26 +117,57 @@ function getRMPLink(firstName, lastName){
     xhttp.open(method, url, true);
     xhttp.send('');
 
-    // A short delay to simply make sure that xhttp.onload() has fully been called --> prevent response from being undefined
-    setTimeout(function(){
-        // Create a temporary div that will act as the holder for retrieving the direct URL for the professors RMP page
-        var div = document.createElement('div');
-        // Remove all (/g) script tags within the response text --> greatly reduces the size of the responseText
-        response = response.replace(/<script(.|\s)*?\/script>/g, '');
-        div.innerHTML = response;
-        // The "listing PROFESSOR" class houses the results on the page of our xhttp request
-        var listingsClass = div.getElementsByClassName("listing PROFESSOR")[0];
-        // Check to make sure that the first element of the HTMLCollection is not undefined
-        if(listingsClass != undefined){
-            // Check to make sure that the first element of HTMLCollection is not undefined
-            listingsClass = listingsClass.getElementsByTagName('a')[0];
+    /*
+        Instantiate a Promise that will handle the return of the professor-specific URL inside of setTimeout.
+        The Promise allows us to return this information without having the function return before the setTimeout is complete.
+    */
+    var promise = new Promise(function(resolve, reject){
+        // A short delay to simply make sure that xhttp.onload() has fully been called --> prevent response from being undefined
+        setTimeout(function(){
+            // Create a temporary div that will act as the holder for retrieving the direct URL for the professors RMP page
+            var div = document.createElement('div');
+            // Remove all (/g) script tags [and their content] within the response text --> greatly reduces the size of the responseText
+            response = response.replace(/<script(.|\s)*?\/script>/g, '');
+            div.innerHTML = response;
+            // The "listing PROFESSOR" class houses the results on the page of our xhttp request
+            var listingsClass = div.getElementsByClassName("listing PROFESSOR")[0];
+            // Check to make sure that the first element of the HTMLCollection is not undefined
             if(listingsClass != undefined){
-                // The specific url is: RMP.com + the href of the listing
-                var specificURL =  "http://www.ratemyprofessors.com" + listingsClass.getAttribute('href');
-                console.log(specificURL);  
+                // Check to make sure that the first element of HTMLCollection is not undefined
+                listingsClass = listingsClass.getElementsByTagName('a')[0];
+                if(listingsClass != undefined){
+                    // The specific url is: RMP.com + the href of the listing
+                    var specificURL =  "http://www.ratemyprofessors.com" + listingsClass.getAttribute('href');
+                    resolve(specificURL);
+                    //console.log(specificURL);  
+                }
+            }else{
+                // Send back an error
+                reject(Error("No RMP link found"));
             }
-        }else{
-            console.log("Error on EMP Listing --> Prof. reviews do not exist?");
-        }
-    }, 500);
+        }, 500);
+    });
+
+    return promise;
+}
+
+/**
+ * Attaches a link (URL) to the given element
+ * @param {object} element 
+ * @param {string} rmpLink 
+ * @param {string} roughText 
+ */
+function modifyElement(element, rmpLink, roughText){
+    // Create and modify a temporary a-tag element that will replace the current roughText with a linked-text
+    var aTag = document.createElement('a');
+    aTag.setAttribute('href', rmpLink);
+    // Adding in target="_blank" makes the link open in a new tab
+    aTag.setAttribute('target', "_blank");
+    aTag.style.fontWeight = 'bold';
+    aTag.style.color = '#0829a0';
+    aTag.innerText = roughText;
+
+    element.textContent = "";
+    element.appendChild(aTag);
+
 }
